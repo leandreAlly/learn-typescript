7b42d95882b85b09a25e3cab8977d12c12183471
allyleandre <tuyambazeleandre@gmail.com>
2023-07-03 12:26:54 +0200

    refactor using alias type

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-const employee: {
+type Employee = {
   readonly id: number;
   name: string;
   retire: (date: Date) => void;
-} = {
+};
+
+const employee: Employee = {
   id: 1,
   name: "Ally",
   retire: (date: Date) => {
