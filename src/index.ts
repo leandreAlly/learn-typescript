ca1dbb5ff8c88777d9faa9638f6c45abca8d147b
allyleandre <tuyambazeleandre@gmail.com>
2023-07-04 18:30:06 +0200

    create class and object

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,22 @@
-// Type assertion
-const phone = <HTMLInputElement>document.getElementById("phone");
+class Account {
+  readonly id: number;
+  owner: string;
+  balance: number;
+  nickname?: string;
 
-phone.value;
+  constructor(id: number, owner: string, balance: number) {
+    this.id = id;
+    this.owner = owner;
+    this.balance = balance;
+  }
+
+  deposit(amount: number) {
+    if (amount < 0) throw new Error("Invalid amount");
+    this.balance += amount;
+  }
+}
+
+let account = new Account(1, "Ally", 0);
+
+account.deposit(1000);
+console.log(account);
