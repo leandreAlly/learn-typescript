790eb6840cf398dcd9be9cbfed5e7bf0b9b590e1
allyleandre <tuyambazeleandre@gmail.com>
2023-07-04 20:04:10 +0200

    working with getters

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,19 @@
 "use strict";
-let mySize = 2;
-console.log(mySize);
+class Account {
+    constructor(id, owner, _balance) {
+        this.id = id;
+        this.owner = owner;
+        this._balance = _balance;
+    }
+    get balance() {
+        return this._balance;
+    }
+    deposit(amount) {
+        if (amount < 0)
+            throw new Error("Invalid amount");
+        this._balance += amount;
+    }
+}
+let account = new Account(1, "Ally", 0);
+account.deposit(1020);
+console.log(account.balance);
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,23 @@
 class Account {
-  readonly id: number;
-  owner: string;
-  balance: number;
   nickname?: string;
 
-  constructor(id: number, owner: string, balance: number) {
-    this.id = id;
-    this.owner = owner;
-    this.balance = balance;
+  constructor(
+    public id: number,
+    public owner: string,
+    private _balance: number
+  ) {}
+
+  get balance(): number {
+    return this._balance;
   }
 
-  deposit(amount: number) {
+  deposit(amount: number): void {
     if (amount < 0) throw new Error("Invalid amount");
-    this.balance += amount;
+    this._balance += amount;
   }
 }
 
 let account = new Account(1, "Ally", 0);
 
-account.deposit(1000);
-console.log(account);
+account.deposit(1020);
+console.log(account.balance);
