944b3a93a95d9770af5c4d8db32f36939b3aed81
allyleandre <tuyambazeleandre@gmail.com>
2023-07-08 06:11:59 +0200

    working with mudule

---
+++ src/module/index.ts
@@ -0,0 +1,5 @@
+import { Circle2 } from "./shapes";
+
+let circle = new Circle2(2);
+
+console.log(circle.radius);
---
+++ src/module/shapes.ts
@@ -0,0 +1,7 @@
+export class Circle2 {
+  constructor(public radius: number) {}
+}
+
+class Square {
+  constructor(public width: number) {}
+}
